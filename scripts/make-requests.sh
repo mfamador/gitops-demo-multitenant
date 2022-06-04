bb5b25b501c1938f59a5b508a17799be9fcce0ea
Marco Amador <amador.marco@gmail.com>
2022-06-04 16:50:28 +0100

    docs: invert color on http 500

--- scripts/make-requests.sh
+++ scripts/make-requests.sh
@@ -9,9 +9,9 @@ do
     http_code=$(tail -n1 <<< "$response")
     content=$(sed '$ d' <<< "$response")
     if [[ $http_code == *"500"* ]]; then
-        echo "\033[0;31m$content"
+        echo "$content" | lolcat --invert
     else
-        if [[ "$content" == *"1.0.0"* ]] || [[ "$content" = *"0.1.0"* ]] ; then
+        if [[ "$content" == *".0.0"* ]] ; then
             echo "$content"
         else
             echo "$content" | lolcat
