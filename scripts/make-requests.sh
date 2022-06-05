047c4652e6409fe9c990b9465874cdffbc678ccd
Marco Amador <amador.marco@gmail.com>
2022-06-05 12:00:41 +0100

    docs: red color on http 500

--- scripts/make-requests.sh
+++ scripts/make-requests.sh
@@ -3,13 +3,16 @@
 URL=${1:-localhost:8000}
 HEADER=${2:-name:value}
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 while true;
 do
     response=$(curl -s -H "$HEADER" -w "%{http_code}" $URL)
     http_code=$(tail -n1 <<< "$response")
     content=$(sed '$ d' <<< "$response")
     if [[ $http_code == *"500"* ]]; then
-        echo "$content" | lolcat --invert
+        echo -e "$RED" "$content" "$NC"
     else
         if [[ "$content" == *".0.0"* ]] ; then
             echo "$content"
